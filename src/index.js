f4eaacbe57cfb1ecbe92d3cb760fdea0a022bc9e
Dimitri De Franciscis <dimitri@megadix.it>
2019-06-28 10:29:14 +0200

    feat: Move route scanning to separate component
    Separate component routeScanner for easier unit testing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-
-const logger = require('./logger');
+const routeScanner = require('./routeScanner');
 
 module.exports.DEFAULT_ROOT_DIR = './routes';
 module.exports.DEFAULT_BASE_PATH = '';
@@ -28,76 +25,9 @@ module.exports.mountRoutes = function (app,
                                        rootDir = module.exports.DEFAULT_ROOT_DIR,
                                        basePath = module.exports.DEFAULT_BASE_PATH,
                                        filter = module.exports.DEFAULT_FILTER) {
-
-    rootDir = rootDir || './routes';
-    basePath = basePath || '';
-    filter = filter || '.js';
-
-    rootDir = _stripTrailingSlash(rootDir);
-    const normalizedRootDir = path.normalize(`${process.cwd()}${path.sep}${rootDir}`);
-    basePath = _addLeadingSlash(_stripTrailingSlash(basePath));
-
-    mountDir(normalizedRootDir);
-
-    function mountDir(dirPath) {
-        logger.debug(`Mounting dir  = ${dirPath}`);
-        fs.readdirSync(dirPath).forEach(filename => {
-            const filePath = `${dirPath}${path.sep}${filename}`;
-            const stat = fs.statSync(filePath);
-            if (stat.isDirectory()) {
-                mountDir(filePath);
-            }
-            else if (stat.isFile() && filename.toLowerCase().endsWith('.js')) {
-                mountFile(filePath);
-            }
-
-        });
-    }
-
-    function mountFile(filePath) {
-        logger.debug(`Mounting file = ${filePath}`);
-        const dirname = path.dirname(filePath);
-        const requestPath = (dirname.startsWith(normalizedRootDir) ? dirname.substr(normalizedRootDir.length) : dirname)
-            .replace(/\\/g, '/'); // Windows-only
-
-        const filename = path.basename(filePath);
-
-        let filePathPart = filename;
-
-        if (filename.indexOf(filter) < 0) {
-            return;
-        }
-
-        // remove pattern from route path
-        filePathPart = filePathPart.replace(filter, '');
-
-        if (filePathPart.endsWith('.js')) {
-            // remove trailing .js if the path matched start of the string
-            filePathPart = filePathPart.substr(0, filePathPart.length - 3);
-        }
-
-        const isIndex = filePathPart.toLowerCase() === 'index';
-
-        // combine path segments
-        const routePath = `${basePath}${requestPath}/${isIndex ? '' : filePathPart}`;
-        app.use(routePath, require(filePath));
-    }
-
+    console.log("POMODORO");
+    const routes = routeScanner.scanRoutes(rootDir, basePath, filter);
+    routes.forEach(route => {
+        app.use(route.path, require(route.src));
+    });
 };
-
-function _cleanString(s) {
-    if (!s) {
-        return '';
-    }
-    return s.trim();
-}
-
-function _stripTrailingSlash(s) {
-    s = _cleanString(s);
-    return s.endsWith('/') ? s.substr(0, s.length - 1) : s;
-}
-
-function _addLeadingSlash(s) {
-    s = _cleanString(s);
-    return s.length > 0 && !s.startsWith('/') ? `/${s}` : s;
-}
---
+++ test/routeScanner.test.js
@@ -0,0 +1,106 @@
+const _ = require('lodash');
+const expect = require('chai').expect;
+const path = require('path');
+const mockFs = require('mock-fs');
+
+const index = require('../src/index');
+const routeScanner = require('../src/routeScanner');
+
+describe('routeScanner', function() {
+  // component under test
+
+  afterEach(function() {
+    mockFs.restore();
+  });
+
+  function verifyRoutes(controllers) {
+    const checkRoutes = new Map();
+    controllers.forEach(controller => {
+      const src = path.relative('./routes', controller.src).replace(/\\/g, '/');
+      checkRoutes.set(controller.path, src);
+      const instance = require(controller.src);
+      expect(instance).not.to.be.null;
+    });
+
+    return checkRoutes;
+  }
+
+  describe('scanRoutes()', function() {
+    it('default parameters', function() {
+
+      mockFs({
+        'routes': {
+          'test-1': {
+            'test-1.1': {
+              'index.js': '',
+              'other.js': ''
+            },
+            'index.js': ''
+          },
+          'test-2': {
+            'controller-1.js': '',
+            'controller-2.js': ''
+          },
+          'index.js': '',
+          'other.js': ''
+        }
+      });
+
+      const controllers = routeScanner.scanRoutes(index.DEFAULT_ROOT_DIR, index.DEFAULT_BASE_PATH, index.DEFAULT_FILTER);
+      expect(controllers.length).to.equal(7);
+
+      const checkRoutes = verifyRoutes(controllers);
+
+      expect(checkRoutes.get('/')).to.equal('index.js');
+      expect(checkRoutes.get('/other')).to.equal('other.js');
+      expect(checkRoutes.get('/test-1/')).to.equal('test-1/index.js');
+      expect(checkRoutes.get('/test-1/test-1.1/')).to.equal('test-1/test-1.1/index.js');
+      expect(checkRoutes.get('/test-1/test-1.1/other')).to.equal('test-1/test-1.1/other.js');
+      expect(checkRoutes.get('/test-2/controller-1')).to.equal('test-2/controller-1.js');
+      expect(checkRoutes.get('/test-2/controller-2')).to.equal('test-2/controller-2.js');
+    });
+
+    describe('custom rootDir and basePath', function() {
+      const doTest = (realRootDir, rootDir, basePath) => () => {
+        mockFs({
+          [realRootDir]: {
+            'test-1': {
+              'test-1.1': {
+                'index.js': '',
+                'other.js': ''
+              },
+              'index.js': ''
+            },
+            'test-2': {
+              'controller-1.js': '',
+              'controller-2.js': ''
+            },
+            'index.js': '',
+            'other.js': ''
+          }
+        });
+
+        const controllers = routeScanner.scanRoutes(rootDir, basePath, index.DEFAULT_FILTER);
+        expect(controllers.length).to.equal(7);
+
+        const checkRoutes = verifyRoutes(controllers);
+
+        expect(checkRoutes.get('/customPath/')).to.equal('../test/sunny-day/index.js');
+        expect(checkRoutes.get('/customPath/other')).to.equal('../test/sunny-day/other.js');
+        expect(checkRoutes.get('/customPath/test-1/')).to.equal('../test/sunny-day/test-1/index.js');
+        expect(checkRoutes.get('/customPath/test-1/test-1.1/')).to.equal('../test/sunny-day/test-1/test-1.1/index.js');
+        expect(checkRoutes.get('/customPath/test-1/test-1.1/other')).to.equal('../test/sunny-day/test-1/test-1.1/other.js');
+        expect(checkRoutes.get('/customPath/test-2/controller-1')).to.equal('../test/sunny-day/test-2/controller-1.js');
+        expect(checkRoutes.get('/customPath/test-2/controller-2')).to.equal('../test/sunny-day/test-2/controller-2.js');
+      };
+
+      it('correct parameters - 1', doTest('test/sunny-day', './test/sunny-day', '/customPath'));
+
+      it('correct parameters - 2', doTest('test/sunny-day', 'test/sunny-day', '/customPath'));
+
+      it('not-so-correct parameters', doTest('test/sunny-day', 'test/sunny-day/', '/customPath/'));
+
+    });
+
+  });
+});
